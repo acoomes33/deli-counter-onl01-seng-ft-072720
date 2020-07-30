8c3120b53406093915dab38ab1a2994e57f0c0bb
Alan Coomes <acoomes1@cord.edu>
2020-07-30 18:53:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else 
+    line_deli = []
+    katz_deli.each_with_index do |name, i|
+       line_deli.push("#{i= i + 1}. #{name}")
+    end
+     line_deli.unshift("The line is currently:")
+     puts line_deli.join(" ")
+  end
+end
+
+def take_a_number(katz_deli, new_customer)
+  
+end
+
+def now_serving(customer)
+  
+end
